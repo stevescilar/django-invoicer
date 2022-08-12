64edfda7b76b3aee5cef3de15c490356eb378f63
stevescilar <stevemuambi@gmail.com>
2022-08-12 12:26:12 +0300

    bug fix

--- invoicerMain/invoicerMain/settings.py
+++ invoicerMain/invoicerMain/settings.py
@@ -33,10 +33,10 @@ CORS_ALLOWED_ORIGINS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES':(
-        'rest_framework.authentication.TokenAuthentication'
+        'rest_framework.authentication.TokenAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES':(
-        'rest_framework.permissions.IsAuthenticated'
+        'rest_framework.permissions.IsAuthenticated',
     )
 }
 
